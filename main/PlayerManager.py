e56d9e01593680d9c1f7dd2c7c7ebc5cf62ef24a
Alex Lee <x.lee@mail.utoronto.ca>
2017-09-05 17:02:33 -0400

    add method to rank + print players by descending win share total

--- main/PlayerManager.py
+++ main/PlayerManager.py
@@ -2,7 +2,7 @@
     their attributes as needed'''
 from Player_class import Player
 from PlayerFactory import *
-
+from collections import OrderedDict
 class PlayerManager:
     '''Maintains a collection of all player instances'''
     def __init__(self):
@@ -19,3 +19,13 @@ class PlayerManager:
     def get_player_with_id(self, id):
         '''return a specifc player'''
         return self.all_players[id]
+
+    def rank_by_win_shares(self):
+        '''print all players, ranked in desc. order by win share total'''
+        temp_players = {}
+        for key, value in self.all_players.items():
+            temp_players[self.all_players[key].get_full_name()] = self.all_players[key].get_total_win_share()
+        
+        ranked = OrderedDict(sorted(temp_players.items(), key = lambda t: t[1], reverse= True))
+        for key, value in ranked.items():
+            print("%s total win share: %.2f" % (key, value))
